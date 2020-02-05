e2a73407be9f274e588f5980b6882be5bb452e07
Ubuntu <ubuntu@ip-172-31-35-147.eu-west-2.compute.internal>
2020-02-05 12:34:10 +0000

    adding nav.php as index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-	echo '<p>Hello World from Pam</p>';
+$mydir = '.';
+$pages = scandir($mydir);
+echo '<div class="menu">';
+foreach($pages as $page){
+if(strpos($page, '.php')){
+$link = $mydir.'/'.$page;
+$safeName = str_replace('.php', '', $page);
+echo '<a href="'.$link.'">'.$safeName.'</a> &nbsp; &nbsp;';
+}
+}
+echo '</div>';
 ?>
